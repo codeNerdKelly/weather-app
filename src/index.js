e12e3a7fc5ea3a78c6cba7a66cd2d17613a1aeca
codeNerdKelly <48159112+codeNerdKelly@users.noreply.github.com>
2022-04-13 12:44:07 -0400

    made forecast images responsive

--- src/index.js
+++ src/index.js
@@ -57,6 +57,7 @@ function displayForecast (response){
       <span>
         <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" 
         id="forecast-icon" 
+        class="img-fluid"
         alt="rainy day icon"/></span>
         <span class="forecast-max" id="forecast-max">${Math.round(forecastDay.temp.max)} </span>
         <span class="forecast-min" id="forecast-min">| ${Math.round(forecastDay.temp.min)}</span>
